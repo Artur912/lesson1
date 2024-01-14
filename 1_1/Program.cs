f9fc95fff0d89cbcb2da781585d0066f9bb0d689
Artur <arturartur4210@mail.ru>
2024-01-14 17:23:11 +0500

    первый урок

---
+++ 1_1/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = 25;  //массивы 
+int b = 5;
+
+if (b * b == a)  //условие 
+    Console.WriteLine("yes"); // ввывод на консоль 
+
+else // усовие
+    Console.WriteLine("no"); // ввывод на консоль 
